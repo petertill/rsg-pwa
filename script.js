a66d84058fb044655223bba43f9896cbcf503dfc
Peter Till <tillpetya20@gmail.com>
2024-07-31 15:38:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function(registration) {  console.log('Excellent, registered with scope: ', registration.scope);});
+
+let installPrompt = null;
+const installButton = document.querySelector("#install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
+
+installButton.addEventListener("click", async () => {
+    if (!installPrompt) {
+      return;
+    }
+    const result = await installPrompt.prompt();
+    console.log(`Install prompt was: ${result.outcome}`);
+    disableInAppInstallPrompt();
+  });
+  
+  function disableInAppInstallPrompt() {
+    installPrompt = null;
+    installButton.setAttribute("hidden", "");
+  }
+
+
+  window.addEventListener("appinstalled", () => {
+    disableInAppInstallPrompt();
+  });
+  
+  function disableInAppInstallPrompt() {
+    installPrompt = null;
+    installButton.setAttribute("hidden", "");
+  }
